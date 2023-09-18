0cb9dc9f467a91c52a085ef1bd91305cbf85bf3a
dophelps <dophelps@gmail.com>
2023-09-17 19:31:34 -0700

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        {" "}
-        This page was coded by Dani Phelps and is {""}
-        <a
-          href="https://github.com/dophelps/react-weather-try1"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div class="container">
+        <h1> Weather App</h1>
+        <footer>
+          {" "}
+          This page was coded by Dani Phelps and is {""}
+          <a
+            href="https://github.com/dophelps/react-weather-try1"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
