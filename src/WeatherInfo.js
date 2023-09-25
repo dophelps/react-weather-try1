f456595496190ad080bbb524e659f8de36364fd0
dophelps <dophelps@gmail.com>
2023-09-25 15:26:37 -0700

    added weatherinfo component

---
+++ src/WeatherInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={weatherData.date} />
+        </li>
+        <li className="text-capitalize"> {weatherData.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit"> ºF</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li> Humidity:{weatherData.humidity} %</li>
+            <li>Wind:{weatherData.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
